5af60f183c9fdb93799b677df722fa02d4a0e318
Mandy-vl <2406484lumich@zuyd.nl>
2025-06-03 18:09:14 +0200

    Theme kan nu aangepast worden tussen dark- en light mode. Toegepast op alle pagina's. Zo ingesteld dat als je de website opnieuw opstart, hij onthoud dat je de vorige keer dark- of light mode hebt gekozen. Dit hoef je dus niet elke keer te selecteren.

--- KE03_INTDEV_SE_2_Base/wwwroot/js/site.js
+++ KE03_INTDEV_SE_2_Base/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+} 
+
+if (darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    if (darkmode !== "active") {
+        enableDarkmode()
+    }
+
+    else {
+        disableDarkmode()
+    }
+});
