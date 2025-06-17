a24df1b005e79794fae730778f8fbad3ecf41bfe
camiel schnackers <camielschnackers1@gmail.com>
2025-06-17 12:56:35 +0200

    - view page aangemaakt voor order history.
    - OrderHistoryController class aangemaakt binnen Controller voor het aanroepen van de index pagina van Order history.
    
    - Layout.cshtml knop order history in navbar gelinkt met de view pagina genaamd OrderHistory

---
+++ KE03_INTDEV_SE_2_Base/Controllers/OrderHistoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace KE03_INTDEV_SE_2_Base.Controllers
+{
+    public class OrderHistoryController : Controller
+
+    {
+
+        public IActionResult Index()
+            {
+                return View();
+            }
+        
+    }
+
+}
